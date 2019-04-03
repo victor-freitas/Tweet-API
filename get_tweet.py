0a19734fed799cf0677a4c54b8b4cb08452163c1
victor-freitas <vitorm3rcy@gmail.com>
2019-04-03 18:52:10 -0300

    Version 0.0.2

--- get_tweet.py
+++ get_tweet.py
@@ -2,11 +2,13 @@ import oauth2
 import json
 import urllib.parse
 
-consumer_key = 'KEY'
-consumer_secret = 'CONSUMER_SECRET'
+#4 informações necessárias para utilização da API
+consumer_key = 'aT0nKsBVycowoKTJ8TgAQx6eO'
+consumer_secret = 'i3dGCJfmBajsNA8D5xhohAG8yb9EQQfQ9XY5cnYN2NgvySVoXk'
 
-token_key = 'TOKEN'
-token_secret = 'SECRET_TOKEN'
+token_key = '1112724515497410560-TV7SQx8dqyQujsAi32YcdPUf8yioSG'
+token_secret = 'QVefzIq6KYvQ4tZPHdE2A8S5NqzNmXlgsXMcWJFHYoZjN'
+
 
 #Informações que dão acesso a nossa conta no Twitter
 consumer = oauth2.Consumer(consumer_key, consumer_secret)
@@ -15,14 +17,14 @@ token = oauth2.Token(token_key, token_secret)
 #Junta as duas informações a cima
 cliente = oauth2.Client(consumer, token)
 
-
-pesquisa = input("Pesquise o Twit: ")
+pesquisa = input('Escreva seu Tweet ou Hashtag (#): ')
+#Transforma para o caracter URL > % 
 pesquisa_avancada = urllib.parse.quote(pesquisa, safe='')
 requisicao = cliente.request('https://api.twitter.com/1.1/search/tweets.json?q=' + pesquisa_avancada)
 
+
 #Transformação para String
 decodificar = requisicao[1].decode()
-
 objeto = json.loads(decodificar)
 twittes = objeto['statuses']
 
@@ -30,4 +32,3 @@ for twit in twittes:
     print(twit['user']['screen_name'])
     print(twit['text'])
     print()
-
--- post_tweet.py
+++ post_tweet.py
@@ -2,11 +2,12 @@ import oauth2
 import json
 import urllib.parse
 
-consumer_key = 'KEY'
-consumer_secret = 'CONSUMER_SECRET'
+consumer_key = 'aT0nKsBVycowoKTJ8TgAQx6eO'
+consumer_secret = 'i3dGCJfmBajsNA8D5xhohAG8yb9EQQfQ9XY5cnYN2NgvySVoXk'
 
-token_key = 'TOKEN'
-token_secret = 'SECRET_TOKEN'
+token_key = '1112724515497410560-TV7SQx8dqyQujsAi32YcdPUf8yioSG'
+token_secret = 'QVefzIq6KYvQ4tZPHdE2A8S5NqzNmXlgsXMcWJFHYoZjN'
+
 
 #Informações que dão acesso a nossa conta no Twitter
 consumer = oauth2.Consumer(consumer_key, consumer_secret)
@@ -17,6 +18,7 @@ cliente = oauth2.Client(consumer, token)
 
 
 pesquisa = input("Poste um Tweet aqui: ")
+#Transforma para o caracter URL > % <
 pesquisa_avancada = urllib.parse.quote(pesquisa, safe='')
 requisicao = cliente.request('https://api.twitter.com/1.1/statuses/update.json?status=' + pesquisa_avancada, method='POST')
 
